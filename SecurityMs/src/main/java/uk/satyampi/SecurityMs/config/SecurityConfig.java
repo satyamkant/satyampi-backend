1e0c9a949d354efc3928d4cda5e5dc44a7f198d4
satyamkant <satyamkant2805@gmail.com>
2024-12-26 22:10:25 +0530

    updating Jenkinsfile

--- SecurityMs/src/main/java/uk/satyampi/SecurityMs/config/SecurityConfig.java
+++ SecurityMs/src/main/java/uk/satyampi/SecurityMs/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5173","https://www.satyampi.uk")); // Allow your frontend origin
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://www.satyampi.uk")); // Allow your frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // HTTP methods
         configuration.setAllowedHeaders(List.of("*")); // Allow all headers
         configuration.setAllowCredentials(true); // Allow credentials (cookies, auth headers, etc.)
